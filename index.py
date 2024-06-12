6b77ff26bf56a996e73a556c3f8d7c0f38dace42
AndresVilladaPoli <andres_villada82202@elpoli.edu.co>
2024-06-12 16:46:35 -0500

    Update index

--- index.py
+++ index.py
@@ -2,64 +2,57 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 import time
 
-input = "El Dorado Bogota"
+input = "Eldorado"
 input1 = "Punta Cana"
+
 
 service = Service("driver/chromedriver.exe")
 bot = webdriver.Chrome(service=service)
-bot.maximize_window
+bot.maximize_window()
 time.sleep(5)
 
 bot.get("https://www.viajesexito.com")
+time.sleep(4)
+bot.get("https://www.viajesexito.com")
+
+
+flightType = bot.find_element(By.XPATH, '//*[@id="paquetesTooltips"]/a')
 time.sleep(3)
+flightType.click()
+time.sleep(2)
 
-TipoVuelo = bot.find_element(By.XPATH, '/html/body/form/div[3]/div/article/div/div[1]/div/div[1]/div/div/div[2]/div[1]/ul/li[3]')
+Origin = bot.find_element(By.XPATH, '//*[@id="CityPredictiveFrom_netactica_airhotel"]')
+Origin.click()
+Origin1 = bot.find_element(By.XPATH, '//*[@id="popUpCityPredictiveFrom_netactica_airhotel"]')
+Origin1.send_keys(input)
+Origin1.send_keys(Keys.ENTER)
+time.sleep(5)
+
+
+destination = bot.find_element(By.XPATH, '//*[@id="CityPredictiveTo_netactica_airhotel"]')
+destination.click()
+destination1 = bot.find_element(By.XPATH, '//*[@id="popUpCityPredictiveTo_netactica_airhotel"]')
+destination1.send_keys(input1)
+destination1.send_keys(Keys.ENTER)
 time.sleep(3)
-TipoVuelo.click()
+'''
+departureDate = bot.find_element(By.XPATH, '//*[@id="Date_netactica_air_hotel"]')
+departureDate.click()
+time.sleep(3)
+departureDate1 = bot.find_element(By.XPATH, '/html/body/div[8]/div[2]/div[2]/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div[5]/div[4]/div[2]/div[1]')
+departureDate1.click()
+'''
+##bot.execute_script("window.ScrollTo(0,1)")
+lodging = bot.find_element(By.XPATH, '//*[@id="txtNumPassengersPaquetesComplete"]')
+lodging.click()
 time.sleep(2)
-
-Origen = bot.find_element(By.XPATH, '/html/body/form/div[3]/div/article/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div/div/div[1]/div/div[1]/div/div/input')
-Origen.click()
-Origen.send_keys(input)
+lodging1 = bot.find_element(By.XPATH, '//*[@id="btbAddRoomtwopaquetes"]')
+lodging1.click()
+lodging2 = bot.find_element(By.XPATH, '//*[@id="btbClosePaxPopup"]')
+lodging2.click()
 time.sleep(2)
-
-Destino = bot.find_element(By.XPATH, '/html/body/form/div[3]/div/article/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div/div/div[1]/div/div[3]/div/div/input')
-Destino.click()
-Destino.send_keys(input1)
-time.sleep(2)
-
-FechaSalida= bot.find_element(By.XPATH, '/html/body/form/div[3]/div/article/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div/div/div[2]/div/div[1]')
-FechaSalida.click()
-time.sleep(2)
-
-DiaSalida= bot.find_element(By.XPATH, '/html/body/div[9]/div[2]/div[2]/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div[5]/div[4]/div[2]/div[1]')
-DiaSalida.click()
-time.sleep(2)
-
-DiaRegreso= bot.find_element(By.XPATH, '/html/body/div[9]/div[2]/div[2]/div[2]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div[5]/div[4]/div[2]/div[1]')
-DiaRegreso.click()
-time.sleep(2)
-
-Habitacion1= bot.find_element(By.XPATH, '/html/body/form/div[3]/div/article/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div/div/div[3]/div/div/div/div/p')
-Habitacion1.click()
-time.sleep(2)
-
-AgregarHabitacion= bot.find_element(By.XPATH, '/html/body/form/div[3]/div/article/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[4]/div[2]/div[1]')
-AgregarHabitacion.click()
-time.sleep(2)
-
-Habitacion2=bot.find_element(By.XPATH, '/html/body/form/div[3]/div/article/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/button')
-Habitacion2.click()
-time.sleep(2)
-
-Precio1 = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[4]/div/div[4]/div/div[2]/div/p[1]/span[2]').text
-print(Precio1)
-
-
-print("")
-print("Los precios de la busqueda son" + Precio1)
-
